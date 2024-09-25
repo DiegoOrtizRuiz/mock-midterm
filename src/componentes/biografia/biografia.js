86ffd4ae9c6a2620dcb559f5f35e9dedf08615c3
DiegoOrtizRuiz <92268879+DiegoOrtizRuiz@users.noreply.github.com>
2024-09-25 15:47:55 +0000

    sdasdadsasd

--- src/componentes/biografia/biografia.js
+++ src/componentes/biografia/biografia.js
@@ -1,15 +1,28 @@
-import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./biografia.css";
+import React, { useEffect, useState } from 'react';
 
 const Biografia = () => {
+
+  const [image, setImage] = useState(null);
+  useEffect(() => {
+  const fetchImage = async () => {
+      const response = await fetch('https://picsum.photos/350'); // Reemplaza con tu URL de la API
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const blob = await response.blob();
+      const imageUrl = URL.createObjectURL(blob);
+      setImage(imageUrl);
+  };fetchImage();
+}, []);
   return (
     <div className="container">
       <div className="row">
-        <div className="col align-items-start">
-          <h1>Imagen perfil</h1>
+        <div className="col align-items-start" id = "user_image">
+        {image && <img src={image} alt="Fetched from API" id="imagen_usuario"/>}
         </div>
-        <div className="col align-items-center" style={{ height: "100vh" }}>
+        <div className="col align-items-center" style={{ height: "100vh" }}  id = "user_info">
           <div className="col text-center">
             <div className="row align-items-start">
               <h1 className="username">username</h1>
