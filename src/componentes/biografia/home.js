54b0633b85931e6908f9fe11fef3320521dbd14c
Diego Ortiz <df.ortizr1@uniandes.edu.co>
2024-09-26 10:48:16 -0500

    meh

--- src/componentes/biografia/home.js
+++ src/componentes/biografia/home.js
@@ -22,6 +22,8 @@ const Home = () => {
         fetchCardData();
     }, []);
 
+   
+
     return (
         <div className="container home-container">
             {/* Cycling Section */}
@@ -30,7 +32,7 @@ const Home = () => {
                 <div className="row">
                     {sessions.map((session, index) => (
                         <div key={index} className="col-md-6 mb-4">
-                            <div className="card">
+                            <div className="card" style={{ backgroundImage: `url(${'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ77cwA4ohMD_vZCfuMbkqYf0nUqcBN8QilQA&s'})` }}>
                                 <div className="card-body">
                                     <h5 className="card-title">Cycling session</h5>
                                     {cardsData[index] ? (
@@ -54,8 +56,8 @@ const Home = () => {
                 <div className="row">
                     {sessions.map((session, index) => (
                         <div key={index} className="col-md-6 mb-4">
-                            <div className="card">
-                                <div className="card-body">
+                            <div className="card" style={{ backgroundImage: `url(${'https://www.iberdrola.com/documents/20125/39940/running-mas-que-moda-746x419.jpg/40c26194-0a55-beff-b5f2-7cc4ca7533ce?t=1704699183011'})` }}>
+                            <div className="card-body">
                                     <h5 className="card-title">Running session</h5>
                                     {cardsData[index] ? (
                                         <>
@@ -78,8 +80,8 @@ const Home = () => {
                 <div className="row">
                     {sessions.map((session, index) => (
                         <div key={index} className="col-md-6 mb-4">
-                            <div className="card">
-                                <div className="card-body">
+                            <div className="card" style={{ backgroundImage: `url(${'https://images.unsplash.com/photo-1534289692684-c02577d5560d?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'})` }}>
+                            <div className="card-body">
                                     <h5 className="card-title">Swimming session</h5>
                                     {cardsData[index] ? (
                                         <>
